5c370b8484a611ded4bcaf00f336fdaf8f42e3ff
Vara <saisrinivasvaraprasadk@gmail.com>
2023-06-29 16:44:58 +0530

    Modified the code to allow accurate parsing of date into JSON

--- AngularUploadFile7/src/app/components/upload-files/upload-files.component.ts
+++ AngularUploadFile7/src/app/components/upload-files/upload-files.component.ts
@@ -46,7 +46,8 @@ export class UploadFilesComponent implements OnInit {
       const workbook = XLSX.read(fileContent, { type: 'binary' });
 
       const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-      const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+      const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1, cellDates: false } as XLSX.Sheet2JSONOpts);
+
 
       const headers: string[] = jsonData[0] as string[];
       jsonData.splice(0, 1);
@@ -55,10 +56,22 @@ export class UploadFilesComponent implements OnInit {
         const rowData = row as string[];
         const obj: any = {};
         headers.forEach((header, index) => {
-          obj[header] = rowData[index];
+          if ((header === 'birthdate') && (typeof rowData[index]=="number")) {
+            // Convert the birthdate to the desired format (e.g., 'dd-mm-yyyy')
+            const dateValue = rowData[index];
+            const excelTimestamp = Number(dateValue);
+            const birthdate = new Date((excelTimestamp - 25569) * 86400 * 1000);
+            const day = birthdate.getDate().toString().padStart(2, '0');
+            const month = (birthdate.getMonth() + 1).toString().padStart(2, '0');
+            const year = birthdate.getFullYear().toString();
+            const formattedBirthdate = `${day}-${month}-${year}`;
+            obj[header] = formattedBirthdate;
+          } else {
+            obj[header] = rowData[index];
+          }
         });
-        return obj;
-      });
+        return obj;
+      });
 
       // Remove blank columns
       const filteredRows = rows.map((row: any) => {
@@ -102,89 +115,3 @@ export class UploadFilesComponent implements OnInit {
     // this.fileInfos = this.uploadService.getFiles();
   }
 }
-
-
-// import { Component, OnInit } from '@angular/core';
-// import { UploadFileService } from 'src/app/services/upload-file.service';
-// import { HttpEventType, HttpResponse } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-// import * as XLSX from 'xlsx';
-
-// @Component({
-//   selector: 'app-upload-files',
-//   templateUrl: './upload-files.component.html',
-//   styleUrls: ['./upload-files.component.css']
-// })
-// export class UploadFilesComponent implements OnInit {
-//   allowedFileTypes: string[] = ['csv', 'xml', 'xlsm', 'xlsx'];
-//   selectedFiles: FileList;
-//   currentFile: File;
-//   progress = 0;
-//   message = '';
-//   fileInfos: Observable<any>;
-
-//   constructor(private uploadService: UploadFileService) { }
-
-//   upload() {
-//     this.progress = 0;
-  
-//     this.currentFile = this.selectedFiles.item(0)!;
-//     const fileExtension = this.currentFile.name.split('.').pop()?.toLowerCase() || '';
-//     const fileName=this.currentFile.name.split('.')[0]||'';
-//     if (!this.allowedFileTypes.includes(fileExtension)) {
-//       this.message = 'Invalid file type. Only XLSX, XLSM, CSV, and XML files are allowed.';
-//       return;
-//     }
-
-
-//     const reader = new FileReader();
-//     reader.onload = (e: any) => {
-//       const fileContent = e.target.result;
-//       const workbook = XLSX.read(fileContent, { type: 'binary' });
-
-//       const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-//       const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-
-//       const headers: string[] = jsonData[0] as string[];
-//       jsonData.splice(0, 1);
-
-//       const rows = jsonData.map((row: unknown) => {
-//         const rowData = row as string[];
-//         const obj: any = {};
-//         headers.forEach((header, index) => {
-//           obj[header] = rowData[index];
-//         });
-//         return obj;
-//       });
-
-//       const jsonString = JSON.stringify(rows);
-
-//       // Continue with the file upload process using the JSON file
-//       this.progress = Math.round((58 / 100!) * 100);
-//       this.uploadService.upload(jsonString,fileName).subscribe(
-//         event => {
-//           console.log(event);
-//           this.progress = 100;
-//           this.message = event.message;
-//         },
-//         err => {
-//           this.progress = 0;
-//           this.message = err.error.message;
-//           this.currentFile = undefined!;
-//           console.log(err);
-//         }
-//       );
-//     };
-
-//     reader.readAsBinaryString(this.currentFile);
-//     this.selectedFiles = undefined!;
-//   }
-
-//   selectFile(event) {
-//     this.selectedFiles = event.target.files;
-//   }
-
-//   ngOnInit() {
-//     // this.fileInfos = this.uploadService.getFiles();
-//   }
-// }
